bc3428ecf87586d1a64467883cffaba561ab77f3
Venkata-Mettela <92840988+Venkata-Mettela@users.noreply.github.com>
2021-11-09 22:37:29 +0530

    Update task.py

--- service/task.py
+++ service/task.py
@@ -1,16 +1,12 @@
-from dao import dbconnection
-class Task:
-    def __init__(self, tid,tname, tdesc, tstatus, tpriority, tnotes, tbm, townid, tcrid, crton, modon):
-        self.tid = tid
-        self.tname = tname
-        self.tdesc = tdesc
-        self.tstatus = tstatus
-        self.tpriority = tpriority
-        self.tnotes = tnotes
-        self.tbm = tbm
-        self.townid = townid
-        self.tcrid = tcrid
-        self.crton = crton
-        self.modon = modon
-         dbconnection.insert(tid, tname, tdesc, tstatus, tpriority, tnotes, tbm, townid, tcrid,
-                         crton, modon)
+def __init__(self,taskid,taskname,tdesc,tstatus,tpriority,tnotes,tbm,townid,tcrid,crton,modon):
+    self.taskid=taskid
+    self.taskname = taskname
+    self.tdesc = tdesc
+    self.tstatus = tstatus
+    self.tpriority = tpriority
+    self.tnotes = tnotes
+    self.tbm = tbm
+    self.townid = townid
+    self.tcrid = tcrid
+    self.crton = crton
+    self.modon = modon
